f8f9739e74adb68eb928114db515a48a5e4f9109
Ryan <84947318+RJ17313@users.noreply.github.com>
2023-10-14 12:42:21 -0400

    login page

---
+++ components/loginCard.jsx
@@ -0,0 +1,36 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Input, Link, toggle } from "@nextui-org/react";
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible} from "react-icons/ai"
+
+export default function LoginCard( props ) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible)
+    return (
+        <>
+            <Card className="max-w-lg min-w-[28rem]">
+                <CardHeader>
+                    <p className="mx-auto text-lg">Login</p>
+                </CardHeader>
+                <Divider></Divider>
+                <CardBody className="flex flex-col gap-4">
+                    <Input color="default" variant="bordered" label="Email"></Input>
+                    <Input color="default" variant="bordered" label="Password" type={isVisible ? "text" : "password"} endContent={
+                        <button className="focus:outline-none text-2xl mt-auto" type="button" onClick={toggleVisibility}>
+                            {isVisible ? (
+                                <AiFillEye className=""></AiFillEye>
+                            ) : (
+                                <AiFillEyeInvisible className="text-default"></AiFillEyeInvisible>
+                            )}
+                        </button>
+                    }></Input>
+                </CardBody>
+                <Divider></Divider>
+                <CardFooter className="flex flex-col gap-y-2">
+                    <Button className="mx-auto" color="primary">Confirm</Button>
+                    <Link color="foreground" href="#test">Don't have an account?</Link>
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import AppNavbar from "@/components/appNavbar";
+import LoginCard from "@/components/loginCard";
+
+export default function SignUpPage(props) {
+  return (
+    <>
+      <div className="flex flex-row justify-center">
+        <LoginCard></LoginCard>
+      </div>
+    </>
+  );
+}
