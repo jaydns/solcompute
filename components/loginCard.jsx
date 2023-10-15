92e3c6510a115a88800478d0f54f68e63c69403e
jayden <github@jayden.email>
2023-10-15 04:40:55 +0000

    yuh

--- components/loginCard.jsx
+++ components/loginCard.jsx
@@ -1,9 +1,9 @@
 import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Input, toggle } from "@nextui-org/react";
 import Link from "next/link"
 import { useState } from "react";
-import { AiFillEye, AiFillEyeInvisible} from "react-icons/ai"
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"
 
-export default function LoginCard( props ) {
+export default function LoginCard(props) {
     const [isVisible, setIsVisible] = useState(false);
 
     const toggleVisibility = () => setIsVisible(!isVisible)
@@ -29,7 +29,7 @@ export default function LoginCard( props ) {
                 <Divider></Divider>
                 <CardFooter className="flex flex-col gap-y-2">
                     <Button className="mx-auto" color="primary">Confirm</Button>
-                    <Link color="foreground" href="/signup">Don't have an account?</Link>
+                    <Link color="foreground" href="/signup">Don&apos;t have an account?</Link>
                 </CardFooter>
             </Card>
         </>
